322fa2037c63f7403805d95e3d14b415638aa24d
yupengtao <yupengtao1024@gmail.com>
2024-02-18 16:25:23 +0800

    建两个nacos服务

---
+++ cloudalibaba-provider-payment9011/src/main/java/this0/com/PaymentMain9011.java
@@ -0,0 +1,13 @@
+package this0.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9011 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9011.class, args);
+    }
+}
