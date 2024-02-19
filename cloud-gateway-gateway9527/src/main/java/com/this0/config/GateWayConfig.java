f79ad9bf5a56dd37121815b8613934ad4b118487
yupengtao <yupengtao1024@gmail.com>
2024-02-19 13:02:43 +0800

    配置类方式进行路由配置 & 动态路由

---
+++ cloud-gateway-gateway9527/src/main/java/com/this0/config/GateWayConfig.java
@@ -0,0 +1,18 @@
+package com.this0.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_this0",
+                r->r.path("")    //function函数
+                        .uri("http://news.baidu.com"));
+        return routes.build();
+    }
+}
